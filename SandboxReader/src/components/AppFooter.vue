fa7ca4c159d779c0739c70cb9318002f9920719b
edavalosanaya <davalosaeduardo@gmail.com>
2022-07-16 10:41:43 -0500

    Added content and made a simple naviation bar

---
+++ SandboxReader/src/components/AppFooter.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <footer>
+    <p>Copyright &copy; 2022 - Vanderbilt OELE Group</p>
+  </footer>
+</template>
+
+<script>
+export default {
+  computed: {
+    year() {
+      return Date().getFullYear()
+    }
+  }
+}
+</script>
+
+<style>
+footer {
+  position: fixed;
+  text-align: center;
+  bottom: 0;
+  width: 100%
+}
+</style>
---
+++ SandboxReader/src/components/AppHeader.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+
+  <header>
+    <div>
+      <nav>
+        <ul>
+          <li><RouterLink to="/">Home</RouterLink></li>
+          <li><RouterLink to="/about">About</RouterLink></li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+
+</template>
+
+<style>
+header {
+  display: flex;
+  border-bottom: 1px solid #ccc;
+  padding: .5rem 1rem;
+}
+
+nav {
+  /* margin-left: auto; */
+}
+
+nav ul {
+  list-style: none;
+}
+
+nav ul li {
+  display: inline-flex;
+  /* margin-left: 1rem; */
+  padding: 0rem 0.5rem;
+}
+</style>
