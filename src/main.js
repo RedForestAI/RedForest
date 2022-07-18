2ccae66a8b15b7c74105180cae8a8f8e60beb10b
Eduardo Davalos <davalosaeduardo@gmail.com>
2022-07-18 17:20:30 -0500

    Added store and removed node_modules that was accidentally added because of TS->JS conversion.

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
 import { createApp } from 'vue'
 import App from '@/App.vue'
 import router from '@/router'
+import store from '@/store'
 
 // Create the app instance
 const app = createApp(App)
 
-// Add tue router instance
+// Add the router instance
 app.use(router)
+
+// Add the store instance
+app.use(store)
 
 // Then mount the app instance
 app.mount('#app')
---
+++ src/services/AreasOfInterest.js
@@ -0,0 +1,5 @@
+export default {
+    takeScreenshot() {
+        return null
+    }
+}
