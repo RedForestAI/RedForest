c846907fb6415dda0d267e9fd2adc5f7c08d2acd
edavalosanaya <davalosaeduardo@gmail.com>
2022-12-19 17:05:57 -0600

    feat(sidebar collapse): Added a collapsable sidebar.

--- src/components/PageContent.vue
+++ src/components/PageContent.vue
@@ -1,8 +1,14 @@
 <template>
   <div id="page-container">
     <div id="sidebar-container">
-      <button class="sidebar-button" v-if="display_sidebar == false"> > </button>
-      <button class="sidebar-button" v-if="display_sidebar == true"> &lt; </button>
+      <div id="sidebar" v-if="display_sidebar == true" :class="{ expanded: display_sidebar }">
+        <h1>Table of Contents</h1>
+        <li v-for="page in pageList" :key="page.id">
+          {{ page.title }}
+        </li>
+      </div>
+      <button class="sidebar-button" v-if="display_sidebar == false" v-on:click="toggleSidebar()"> > </button>
+      <button class="sidebar-button" :class="{ expanded_button: display_sidebar }" v-if="display_sidebar == true" v-on:click="toggleSidebar()"> &lt; </button>
     </div>
     <div id="html-container">
       <div v-html="html" class="load-html"></div>
@@ -21,8 +27,14 @@ export default {
   data() {
     return {
       pageFilepath: '/content/climate_change/pages/introduction.html',
+      pageList: [{title: "Page 1"}, {title: "Page 2"}, {title: "Page 3"}],
       html: null,
-      display_sidebar: true
+      display_sidebar: false
+    }
+  },
+  methods: {
+    toggleSidebar: function() {
+      this.display_sidebar = !this.display_sidebar
     }
   },
   mounted () {
@@ -48,16 +60,32 @@ img {
 #sidebar-container {
   align-items: stretch;
 }
+#sidebar {
+  position: absolute;
+  z-index: 15;
+}
 #html-container {
   flex: 80%;
   padding: 1em;
+  padding-left: 2em;
 }
 #page-buttons {
   margin-top: 5em;
 }
 
+.expanded {
+  background: none no-repeat scroll 0 0 #fff;
+  width: 400px;
+  height: 100%;
+}
+.expanded_button {
+  left: 400px;
+}
+
 .sidebar-button {
   height: 100%;
+  position: absolute;
+  z-index: 15;
 }
 .load-html {
   overflow: auto;
