46c0ff2df83517a5a4f846d0aa1b2044ae425985
edavalosanaya <davalosaeduardo@gmail.com>
2023-02-03 07:57:09 -0600

    feat(ChimeraPy): Started testing the ChimeraPy integrator.

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import App from '@/App.vue'
 import router from '@/views/router' // Views
 import { createPinia } from 'pinia' // State management
 import { AOIWebTracker } from 'aoiwebtracker' // AOI tracker
+import ChimeraJSIntegrator from 'chimera-js-integrator' // ChimeraPy integrator
 import VuePdf from 'vue3-pdfjs' // PDF
 import emitter from './emitter'
 
@@ -15,37 +16,49 @@ import { useMainStore } from '@/store/MainStore'
 // Create pinia
 const pinia = createPinia()
 const store = useMainStore(pinia)
+    
+// Install ChimeraPy integration
+const chimerajs = new ChimeraJSIntegrator()
+chimerajs.install({
+  emitter: emitter,
+  eventArray: [],
+  subPort: 7777,
+  subIP: '127.0.0.1',
+  repPort: 6767
+})
 
 // Initialized store
 const initPromise = store.initialize()
 
 // Only execute the following once the store had loaded
 initPromise.then(
-    (value) => {
-        console.log("Init Promise: " + value)
+  (value) => {
+    console.log("Init Promise: " + value)
 
-        // Create application
-        const app = createApp(App)
+    // Create application
+    const app = createApp(App)
 
-        // Install store first (as others depend on it)
-        app.use(pinia)
+    // Install store first (as others depend on it)
+    app.use(pinia)
 
-        // Setting up global parameters
-        app.config.globalProperties.$mitt = emitter
+    // Setting up global parameters
+    app.config.globalProperties.$mitt = emitter
+    // app.config.globalProperties.zmq = zmq
+    // app.config.globalProperties.$chimerajs = chimerajs
 
-        // Then install others
-        app.use(router)
-            .use(AOIWebTracker, { 
-                drawCanvas: false,
-                toTrackElements: [
-                    {tag: 'div', id: 'scr-page-container', recursive: true, wordLevel: true},
-                    {tag: 'div', id: 'scr-quiz-container', recursive: true, wordLevel: true},
-                    {tag: 'div', id: 'scr-nav', recursive: true, wordLevel: true}
-                ]
-            })
-            .use(VuePdf)
+    // Then install others
+    app.use(router)
+      .use(AOIWebTracker, { 
+        drawCanvas: false,
+        toTrackElements: [
+          {tag: 'div', id: 'scr-page-container', recursive: true, wordLevel: true},
+          {tag: 'div', id: 'scr-quiz-container', recursive: true, wordLevel: true},
+          {tag: 'div', id: 'scr-nav', recursive: true, wordLevel: true}
+        ]
+        })
+      .use(VuePdf)
 
-        // Finish mounting the application
-        app.mount('#app')
-    }
+    // Finish mounting the application
+    app.mount('#app')
+  }
 )
