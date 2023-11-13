7bae2a8f19d01a3e7b8c8761fe294c7155fdc9e7
edavalosanaya <davalosaeduardo@gmail.com>
2023-11-13 08:58:43 -0600

    Adding tab title.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import ProtectedRoute from './utils/ProtectedRoute';
 
 // Pages
@@ -8,26 +9,34 @@ import DashboardPage from './pages/DashboardPage';
 import RegisterPage from './pages/RegisterPage';
 import VerifyEmailPage from './pages/VerifyEmailPage';
 import AssignmentPage from './pages/AssignmentPage';
-import NotFoundPage from './pages/NotFoundPage'
+import NotFoundPage from './pages/NotFoundPage';
 
-const App: React.FC = () => (
-	<BrowserRouter>
-		<Routes>
-      <Route path="/" element=<HomePage />/>
-      <Route path="/login" element=<LoginPage />/>
-      <Route path="/register" element=<RegisterPage />/>
-      <Route path="/verify-email" element=<VerifyEmailPage />/>
-      <Route path='/user/*' element={<ProtectedRoute />}>
-        <Route index element=<DashboardPage />/>
-        <Route path='assignments/:id' element=<AssignmentPage />/>
+const App: React.FC = () => {
+
+  useEffect(() => {
+    // Set the title when the location (route) changes
+    document.title = 'SandCastleReader';
+  }, []);
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/verify-email" element={<VerifyEmailPage />} />
+        <Route path="/user/*" element={<ProtectedRoute />}>
+          <Route index element={<DashboardPage />} />
+          <Route path="assignments/:id" element={<AssignmentPage />} />
+          {/* Catch-all route for 404 page */}
+          <Route path="*" element={<NotFoundPage />} />
+        </Route>
+
         {/* Catch-all route for 404 page */}
-        <Route path="*" element=<NotFoundPage />/>
-      </Route>
-
-      {/* Catch-all route for 404 page */}
-      <Route path="*" element=<NotFoundPage />/>
-		</Routes>
-	</BrowserRouter>
-);
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default App;
