91e010150644e9fdf0e6d65e53affdb2ba56f16f
edavalosanaya <davalosaeduardo@gmail.com>
2023-11-14 22:19:31 -0600

    Made webgazer controls.

---
+++ src/contexts/webgaze/WebGazerManager.js
@@ -0,0 +1,90 @@
+let instance = null;
+
+export class WebGazerManager {
+  constructor() {
+    if (!instance) {
+      this.loadScript();
+      instance = this;
+    }
+
+    return instance;
+  }
+
+  loadScript() {
+    const script = document.createElement('script');
+    script.src = 'https://webgazer.cs.brown.edu/webgazer.js'; // Adjust the path to your local script
+    script.onload = () => {
+      this.isLoaded = true;
+      this.isActive = false;
+      this.isListening = false;
+    };
+    script.onerror = () => {
+      console.error('Error loading WebGazer.js');
+    };
+    document.head.appendChild(script);
+  }
+
+  setupWebGazer() {
+    if (this.isLoaded && !this.isActive) {
+      window.webgazer.setGazeListener((data, elapsedTime) => {
+        if (data) {
+          this.gazeData = window.webgazer.util.bound(data);
+        }
+      }).begin();
+      this.hide();
+      this.isActive = true;
+      this.isListening = true;
+    }
+  }
+  
+  start() {
+    if (this.isLoaded) {
+      if (!this.isListening) {
+        this.setupWebGazer(); 
+      }
+      else if (!this.isActive) {
+      window.webgazer && window.webgazer.resume();
+      this.isActive = true;
+      }
+    }
+  }
+
+  show() {
+    window.webgazer && window.webgazer
+      .showPredictionPoints(true)
+      .showVideo(true);
+  }
+
+  hide() {
+    window.webgazer && window.webgazer
+      .showPredictionPoints(false)
+      .showVideo(false);
+  }
+
+  stop() {
+    if (this.isActive) {
+      window.webgazer && window.webgazer.end();
+      this.hide();
+      this.isActive = false;
+    }
+  }
+
+  end() {
+    if (this.isActive) {
+      window.webgazer && window.webgazer.end();
+      window.webgazer && window.webgazer.stopVideo();
+      this.isActive = false;
+    }
+  }
+
+  getGazeData() {
+    return this.gazeData;
+  }
+}
+
+export const getWebGazerInstance = () => {
+  if (!instance) {
+    instance = new WebGazerManager();
+  }
+  return instance;
+};
--- src/pages/AssignmentPage.tsx
+++ src/pages/AssignmentPage.tsx
@@ -3,21 +3,39 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import PDFViewer from '../components/PDFViewer';
 import ResponsiveAppBar from '../components/ResponsiveAppBar';
+import { getWebGazerInstance } from '../contexts/webgaze/WebGazerManager'
 import './css/AssignmentPage.css';
-
-import { WebGazeProvider } from '../components/webgaze/WebGazeContext';
 
 const AssignmentPage = () => {
   const { id } = useParams<{ id: string }>();
+  const webGazer = getWebGazerInstance();
+
+    const handleStart = () => {
+      webGazer.start();
+    };
+    
+    const handleHide = () => {
+      webGazer.hide();
+    };
+    
+    const handleShow = () => {
+      webGazer.show();
+    };
+
+    const handleStop = () => {
+      webGazer.stop();
+    };
 
   return (
     <div className="assignment-page">
-      <WebGazeProvider>
-        <ResponsiveAppBar />
-        <div className='pdf-viewer-container'>
-          <PDFViewer />
-        </div>
-      </WebGazeProvider>
+      <ResponsiveAppBar />
+      <div className='pdf-viewer-container'>
+        <PDFViewer />
+      </div>
+      <button onClick={handleStart}>Start WebGazer</button>
+      <button onClick={handleShow}>Show WebGazer</button>
+      <button onClick={handleHide}>Hide WebGazer</button>
+      <button onClick={handleStop}>Stop WebGazer</button>
     </div>
   );
 };
