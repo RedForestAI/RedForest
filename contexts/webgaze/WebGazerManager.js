ca28c0915f82b3f406f9992944a93a35e6ea3a4c
edavalosanaya <davalosaeduardo@gmail.com>
2023-11-16 08:25:42 -0600

    Made changes to make WebGazer compatible with NextJS.

--- contexts/webgaze/WebGazerManager.js
+++ contexts/webgaze/WebGazerManager.js
@@ -5,34 +5,48 @@ export class WebGazerManager {
     if (!instance) {
       instance = this;
       this.scriptId = 'webgazer-script';
-      this.loadScript();
+      this.scriptLoaded = false;
+      this.isActive = false;
+      // Only load script if in the browser environment
+      if (typeof window !== "undefined") {
+        this.loadScript();
+      }
     }
-
     return instance;
   }
 
   removeExistingScript() {
-    const existingScript = document.getElementById(this.scriptId);
-    if (existingScript) {
-      existingScript.remove();
+    // Ensure this runs only in the browser
+    if (typeof document !== "undefined") {
+      const existingScript = document.getElementById(this.scriptId);
+      if (existingScript) {
+        existingScript.remove();
+      }
+      if (typeof window !== "undefined") {
+        window.webgazer = null; // Clear the existing webgazer object
+      }
     }
-    window.webgazer = null; // Clear the existing webgazer object
   }
 
   loadScript() {
     this.removeExistingScript();
 
-    return new Promise((resolve, reject) => {
-      const script = document.createElement('script');
-      script.id = this.scriptId;
-      script.src = 'https://webgazer.cs.brown.edu/webgazer.js';
-      script.onload = () => resolve();
-      script.onerror = () => {
-        console.error('Error loading WebGazer.js');
-        reject();
-      };
-      document.head.appendChild(script);
-    });
+    if (typeof document !== "undefined") {
+      return new Promise((resolve, reject) => {
+        const script = document.createElement('script');
+        script.id = this.scriptId;
+        script.src = 'https://webgazer.cs.brown.edu/webgazer.js';
+        script.onload = () => {
+          this.scriptLoaded = true;
+          resolve();
+        };
+        script.onerror = () => {
+          console.error('Error loading WebGazer.js');
+          reject();
+        };
+        document.head.appendChild(script);
+      });
+    }
   }
 
   async setupWebGazer() {
@@ -65,33 +79,38 @@ export class WebGazerManager {
   }
 
   show() {
-    window.webgazer && window.webgazer
-      .showPredictionPoints(true)
-      .showVideo(true);
+    if (this.isActive && window.webgazer) {
+      window.webgazer
+        .showPredictionPoints(true)
+        .showVideo(true);
+    }
   }
 
   hide() {
-    window.webgazer && window.webgazer
-      .showPredictionPoints(false)
-      .showVideo(false);
+    if (this.isActive && window.webgazer) {
+      window.webgazer
+        .showPredictionPoints(false)
+        .showVideo(false);
+    }
   }
 
   stop() {
-    if (this.isActive) {
-      window.webgazer && window.webgazer.pause();
+    if (this.isActive && window.webgazer) {
+      window.webgazer.pause();
       this.isActive = false;
     }
   }
 
   end() {
-    window.webgazer && window.webgazer.stopVideo();
-    window.webgazer && window.webgazer.end();
-    this.isActive = false;
-    this.isListening = false;
-    this.isLoaded = false;
-  }
-
-  getGazeData() {
-    return this.gazeData;
+    try {
+      if (window.webgazer) {
+        window.webgazer.stopVideo();
+        window.webgazer.end();
+        this.isActive = false;
+        this.scriptLoaded = false;
+      }
+    } catch (error) {
+      console.error('Error in ending WebGazer:', error);
+    }
   }
 }
