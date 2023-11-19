feeca45126fdbf635e1af27bd55412153719cfa1
edavalosanaya <davalosaeduardo@gmail.com>
2023-11-19 15:38:34 -0600

    Corrected NavBar.

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -4,15 +4,22 @@ import Link from "next/link";
 import React, { useState } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
 
+interface NavLink {
+  id: number;
+  link: string;
+  title: string;
+}
+
 const Navbar = () => {
     const [nav, setNav] = useState(false);
 
-  const links = [
+  const links: NavLink[] = [
     {
       id: 1,
-      link: "login",
+      link: "auth/login",
+      title: "Login"
     }
-  ];
+  ]
 
   return (
     <div className="flex justify-between items-center w-full h-14 px-4 text-white bg-zinc-800 nav">
@@ -26,12 +33,12 @@ const Navbar = () => {
       </div>
 
       <ul className="hidden md:flex">
-        {links.map(({ id, link }) => (
+        {links.map(({ id, link, title }) => (
           <li
             key={id}
             className="nav-links px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 hover:text-white duration-200 link-underline"
           >
-            <Link href={link}>{link}</Link>
+            <Link href={link}>{title}</Link>
           </li>
         ))}
       </ul>
@@ -45,13 +52,13 @@ const Navbar = () => {
 
       {nav && (
         <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500">
-          {links.map(({ id, link }) => (
+          {links.map(({ id, link, title }) => (
             <li
               key={id}
               className="px-4 cursor-pointer capitalize py-6 text-4xl"
             >
               <Link onClick={() => setNav(!nav)} href={link}>
-                {link}
+                {title}
               </Link>
             </li>
           ))}
