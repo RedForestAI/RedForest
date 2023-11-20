96cc1917d05e13b7993f44b90f302abe5f6d3425
edavalosanaya <davalosaeduardo@gmail.com>
2023-11-19 18:28:40 -0600

    Added signup page.

--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -1,30 +1,29 @@
 import React from 'react';
-import Link from 'next/link';
+import { createServerClient } from '@supabase/ssr';
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
 
-const SignUpPage = () => {
-  return (
-    <div className="flex justify-center items-center h-screen bg-zinc-950">
-      <div className="p-6 max-w-sm w-full bg-white rounded-lg border border-gray-200 shadow-md">
-        <h2 className="mb-4 text-xl font-bold text-gray-900">Sign Up</h2>
-          <form>
-            <div className="mb-4">
-              <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">Email</label>
-              <input type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="name@example.com" required />
-            </div>
-            <div className="mb-4">
-              <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900">Password</label>
-              <input type="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required />
-            </div>
-            <button type="submit" className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Submit</button>
-          </form>
-          <div className="flex justify-between items-center mt-4">
-          <Link href="login">
-            <button className="text-sm text-blue-600 hover:underline">Already have an account? Sign In</button>
-          </Link>
-        </div>
-      </div>
-    </div>
-  );
-};
 
-export default SignUpPage;
+import SignUp from '../../../components/auth/signup'
+
+export default async function SignUpPage() {
+  const cookieStore = cookies(cookies)
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL, 
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY, 
+    {
+      cookies: {
+        get(name: string) {
+          return cookieStore.get(name)?.value
+        },
+      },
+    }
+  )
+  const { data } = await supabase.auth.getSession();
+
+  if (data?.session) {
+    redirect('/student')
+  }
+
+  return <SignUp />
+}
