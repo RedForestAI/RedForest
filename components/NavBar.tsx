b76c6a3994c56a20845e6b6c8634dd556dad37da
edavalosanaya <davalosaeduardo@gmail.com>
2023-11-20 11:08:55 -0600

    Corrected paths to forgot-password and reset-password.

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -66,21 +66,6 @@ const Navbar = () => {
       >
         {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
       </div>
-
-      {nav && (
-        <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500">
-          {links.map(({ id, link, title }) => (
-            <li
-              key={id}
-              className="px-4 cursor-pointer capitalize py-6 text-4xl"
-            >
-              <Link onClick={() => setNav(!nav)} href={link}>
-                {title}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      )}
     </div>
   );
 };
