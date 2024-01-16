b8c26e10cf86e13d117da4f609c2de6265822e34
edavalosanaya <davalosaeduardo@gmail.com>
2024-01-16 14:04:02 -0600

    Made the flow for STUDENT to become TEACHER.

---
+++ src/app/access/account/_components/DangerZoneForm.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { Profile } from "@prisma/client";
+import { useRouter } from "next/navigation";
+import { api } from "~/trpc/react";
+
+type DangerZoneFormProps = {
+  profile: Profile
+}
+
+export default function DangerZoneForm(props: DangerZoneFormProps) {
+  const accountConvert = api.auth.convertToTeacher.useMutation()
+  const router = useRouter();
+
+  const convertToTeacher = async () => {
+    try {
+      await accountConvert.mutateAsync({profileId: props.profile.id})
+      console.log("Converted to teacher")
+      router.refresh();
+    } catch (error: any) {
+      console.log("Failed to convert to teacher: " + error?.message)
+    }
+  }
+
+  return (
+    <>
+      <h1 className="text-xl font-bold mb-4 text-rose-500 rounded-md">DangerZone</h1>
+      <div className="border-2 rounded-xl border-rose-500 p-4">
+        {props.profile.role == "STUDENT" &&
+          <div>
+            <p className="mb-4">Convert your account from STUDENT to TEACHER. This will delete all course enrollments and user information.</p>
+            <button onClick={convertToTeacher} className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"> Convert to TEACHER </button>
+          </div>
+        }
+        <p className="mt-4 mb-4">Beware that deleting your account is permanent and will delete all your courses and user information.</p>
+        <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"> Delete Account </button>
+      </div>
+    </>
+  )
+}
--- src/server/api/routers/auth.ts
+++ src/server/api/routers/auth.ts
@@ -1,3 +1,5 @@
+import { Role } from "@prisma/client";
+import { z } from "zod";
 import { createTRPCRouter, privateProcedure } from "~/server/api/trpc";
 
 export const authRouter = createTRPCRouter({
@@ -11,4 +13,31 @@ export const authRouter = createTRPCRouter({
       },
     });
   }),
+  convertToTeacher: privateProcedure
+    .input(z.object({ profileId: z.string() }))
+    .mutation(async ({ input, ctx }) => {
+      // Authenticated user and given profileID
+      if (!ctx.user.data.user || input.profileId !== ctx.user.data.user.id) {
+        throw new Error("User is not authenticated");
+      }
+
+      // Confirm that the user is a student
+      const student = await ctx.db.profile.findUniqueOrThrow({
+        where: { id: input.profileId },
+      });
+      if (student.role !== "STUDENT") {
+        throw new Error("User is not a student");
+      }
+
+      // First we need to delete all course Enrollments
+      await ctx.db.courseEnrollment.deleteMany({
+        where: { studentId: input.profileId },
+      });
+
+      // Then convert the user to a teacher
+      return await ctx.db.profile.update({
+        where: { id: input.profileId },
+        data: { role: Role.TEACHER },
+      });
+    })
 });
