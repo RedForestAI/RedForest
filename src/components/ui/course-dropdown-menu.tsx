d394eca98caae591f8b069daa75a4a13678780ac
edavalosanaya <davalosaeduardo@gmail.com>
2024-01-16 12:06:08 -0600

    Added options to delete courses.

---
+++ src/components/ui/course-dropdown-menu.tsx
@@ -0,0 +1,96 @@
+"use client";
+
+import { Course } from '@prisma/client'
+import { Menu, Transition } from '@headlessui/react'
+import { Fragment, useEffect, useRef, useState } from 'react'
+import { ChevronDownIcon } from '@heroicons/react/20/solid'
+import { api } from "~/trpc/react";
+import { useRouter } from "next/navigation";
+
+interface CourseDropDownProps {
+  courseId: string
+  teacherId: string
+}
+
+export default function DropDown( props: CourseDropDownProps ) {
+  const router = useRouter();
+  const deleteMutation = api.course.delete.useMutation()
+
+  const deleteCourse = async () => {
+    try {
+      await deleteMutation.mutateAsync({courseId: props.courseId, teacherId: props.teacherId})
+      router.refresh();
+    } catch (error: any) {
+      console.log(error)
+    }
+  }
+
+  return (
+    <div className="top-16 w-56 text-right">
+      <Menu as="div" className="relative inline-block text-left">
+        <div>
+          <Menu.Button className="inline-flex w-full justify-center rounded-md bg-black/20 px-4 py-2 text-sm font-medium text-white focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75">
+            Options
+            <ChevronDownIcon
+              className="-mr-1 ml-2 h-5 w-5 text-violet-200"
+              aria-hidden="true"
+            />
+          </Menu.Button>
+        </div>
+        <Transition
+          as={Fragment}
+          enter="transition ease-out duration-100"
+          enterFrom="transform opacity-0 scale-95"
+          enterTo="transform opacity-100 scale-100"
+          leave="transition ease-in duration-75"
+          leaveFrom="transform opacity-100 scale-100"
+          leaveTo="transform opacity-0 scale-95"
+        >
+          <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none">
+            <div className="px-1 py-1">
+              <Menu.Item>
+                {({ active }) => (
+                  <button
+                      onClick={deleteCourse}
+                      className={`${
+                        'text-gray-900'
+                      } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                    >
+                    <DeleteInactiveIcon
+                      className="mr-2 h-5 w-5 text-violet-400"
+                      aria-hidden="true"
+                    />
+                    Delete
+                  </button>
+                )}
+              </Menu.Item>
+            </div>
+          </Menu.Items>
+        </Transition>
+      </Menu>
+    </div>
+  )
+}
+
+function DeleteInactiveIcon(props: any) {
+  return (
+    <svg
+      {...props}
+      viewBox="0 0 20 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect
+        x="5"
+        y="6"
+        width="10"
+        height="10"
+        fill="#EDE9FE"
+        stroke="#A78BFA"
+        strokeWidth="2"
+      />
+      <path d="M3 6H17" stroke="#A78BFA" strokeWidth="2" />
+      <path d="M8 6V4H12V6" stroke="#A78BFA" strokeWidth="2" />
+    </svg>
+  )
+}
