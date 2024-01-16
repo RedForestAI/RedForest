b86fc59c3733a176ec79a3b41b4a0354d0e505ab
Eduardo Davalos Anaya <40870026+edavalosanaya@users.noreply.github.com>
2024-01-16 14:33:13 -0600

    Added flow for converting STUDENT to TEACHER (#64)
    * Made the flow for STUDENT to become TEACHER.
    
    * Added method for deleting accounts.
    
    * Addressing env issues.

--- src/app/access/account/page.tsx
+++ src/app/access/account/page.tsx
@@ -4,26 +4,37 @@ import NavBar from '@/components/ui/navbar';
 import { cookies } from 'next/headers'
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import LogoutButton from '@/components/FormInput/LogoutButton';
+import { api } from '~/trpc/server';
+import { Profile } from '@prisma/client';
+import DangerZoneForm from './_components/DangerZoneForm';
 
 export default async function Account() {
   // Fetch data
   const cookieStore = cookies()
   const supabase = createServerComponentClient({ cookies: () => cookieStore })
   const { data } = await supabase.auth.getUser();
+  let profile: Profile = await api.auth.getProfile.query();
 
   return (
     <div>
-      <NavBar includeBurger={true} accountLink={"/account"} logoLink={"/access"}/>
+      <NavBar includeBurger={true} accountLink={"/access/account"} logoLink={"/access"}/>
       <div className="container mx-auto p-4">
-        <h1 className="text-xl font-bold mb-4">Account Settings</h1>
         {data && data.user
-          ? <div className="mb-4">
-            <label className="block text-gray-200 text-sm font-bold mb-2">Email</label>
-            <input type="email" value={data.user?.email} className="shadow appearance-none border rounded w-full py-2 px-3 text-white-700 leading-tight focus:outline-none focus:shadow-outline" disabled />
-          </div>
+          ? <>
+            <h1 className="text-xl font-bold mb-4">Account</h1>
+            <div className="mb-4">
+              <label className="block text-gray-200 text-sm font-bold mb-2">Email</label>
+              <input type="email" value={data.user?.email} className="shadow appearance-none border rounded w-full py-2 px-3 text-white-700 leading-tight focus:outline-none focus:shadow-outline" disabled />
+              <label className="block text-gray-200 text-sm font-bold mb-2 mt-4">Role</label>
+              <input type="email" value={profile.role.toString()} className="shadow appearance-none border rounded w-full py-2 px-3 text-white-700 leading-tight focus:outline-none focus:shadow-outline" disabled />
+            </div>
+            <DangerZoneForm profile={profile}/>
+          </>
           : <div className="mt-4 text-red-600">Failed to fetch account information</div>
         }
-      <LogoutButton />
+      <div className="mt-4">
+        <LogoutButton />
+      </div>
       </div>
     </div>
   );
--- src/env.mjs
+++ src/env.mjs
@@ -17,6 +17,7 @@ export const env = createEnv({
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
+    SUPABASE_SERVICE_KEY: z.string().min(1),
     DIRECT_URL: z.string().min(1),
   },
 
@@ -28,7 +29,6 @@ export const env = createEnv({
   client: {
     NEXT_PUBLIC_SUPABASE_URL: z.string().url(),
     NEXT_PUBLIC_SUPABASE_ANON_KEY: z.string().min(1),
-    NEXT_PUBLIC_UMAMI_WEBSITE_ID: z.string().optional(),
   },
 
   /**
@@ -41,7 +41,7 @@ export const env = createEnv({
     DIRECT_URL: process.env.DIRECT_URL,
     NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
     NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
-    NEXT_PUBLIC_UMAMI_WEBSITE_ID: process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID,
+    SUPABASE_SERVICE_KEY: process.env.SUPABASE_SERVICE_KEY,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
