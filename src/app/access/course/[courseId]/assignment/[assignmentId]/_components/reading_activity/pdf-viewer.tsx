75f0cb098665e7717305cc9a3c4e2e331398c464
edavalosanaya <davalosaeduardo@gmail.com>
2024-01-18 06:55:11 -0600

    Added the annotation and text layers.

--- src/app/access/course/[courseId]/assignment/[assignmentId]/_components/reading_activity/pdf-viewer.tsx
+++ src/app/access/course/[courseId]/assignment/[assignmentId]/_components/reading_activity/pdf-viewer.tsx
@@ -2,8 +2,17 @@
 import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/20/solid";
 import { useEffect, useRef, useState } from "react";
 import { Document, Page, pdfjs } from "react-pdf";
+import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
+import 'react-pdf/dist/esm/Page/TextLayer.css';
 
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+// pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  'pdfjs-dist/build/pdf.worker.min.js',
+  import.meta.url,
+).toString();
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
 
 type PDFViewerProps = {
   file: string;
@@ -54,8 +63,8 @@ export default function PDFViewer(props: PDFViewerProps) {
               className="mb-4"
               key={`page_${index + 1}`}
               pageNumber={index+1}
-              renderAnnotationLayer={false}
-              renderTextLayer={false}
+              renderAnnotationLayer={true}
+              renderTextLayer={true}
               onLoadSuccess={onPageLoadSuccess}
               onRenderError={() => setLoading(false)}
               width={Math.max(pageWidth * 0.5, 390)}
