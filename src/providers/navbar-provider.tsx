466a34708288f9472907140904be9414785cffa7
edavalosanaya <davalosaeduardo@gmail.com>
2024-01-26 22:44:31 -0600

    Addressed vercel errors.

--- src/providers/navbar-provider.tsx
+++ src/providers/navbar-provider.tsx
@@ -1,13 +1,14 @@
 import React, { useState, createContext } from 'react';
 
-export const NavBarContext = createContext<React.PureComponent | null>(null);
-export const useNavBarContext = createContext<any>(null);
+export const NavBarContext = createContext<React.ReactNode>(<></>);
+export const useNavBarContext = createContext<any>(undefined);
 
 export const NavBarProvider = (props: { children: any }) => {
-  const [navBarContent, setNavBarContent] = useState<React.PureComponent | null>(null);
+  const [navBarContent, setNavBarContent] = useState<React.ReactNode>(<></>);
 
   return (
     <NavBarContext.Provider value={navBarContent}>
+      {/* @ts-ignore */}
       <useNavBarContext.Provider value={{ setNavBarContent }}>
         {props.children}
       </useNavBarContext.Provider>
