f958a0108b665776e3af6e7cdc6cb75571afa3a6
edavalosanaya <davalosaeduardo@gmail.com>
2024-01-31 19:55:35 -0600

    Address vercel errors.

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,19 +1,16 @@
 "use client";
 
 import { MiddleNavBarProvider, EndNavBarProvider } from "./navbar-provider";
-import { WebGazerProvider } from "./WebGazerContext"
 import React, { type PropsWithChildren } from "react";
 
 export function Providers({ children }: PropsWithChildren) {
   return (
     <>
-      {/* <WebGazerProvider> */}
-        <MiddleNavBarProvider>
-          <EndNavBarProvider>
-                {children}
-          </EndNavBarProvider>
-        </MiddleNavBarProvider>
-      {/* </WebGazerProvider> */}
+      <MiddleNavBarProvider>
+        <EndNavBarProvider>
+          {children}
+        </EndNavBarProvider>
+      </MiddleNavBarProvider>
     </>
   );
 }
