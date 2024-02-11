07c0a814cb7892e111e3318079422992ff5ca29c
edavalosanaya <davalosaeduardo@gmail.com>
2024-02-11 10:58:15 -0600

    Minor cosmetic improvement.

--- src/app/access/course/[courseId]/assignment/[assignmentId]/_components/reading_activity/blur-modal.tsx
+++ src/app/access/course/[courseId]/assignment/[assignmentId]/_components/reading_activity/blur-modal.tsx
@@ -2,7 +2,7 @@
 export default function BlurModal(props: { onContinue: () => void }) {
   return (
     <div className="fixed top-0 left-0 w-full h-full z-40">
-      <div className="mt-20 w-[50%] h-[50%] m-auto">
+      <div className="mt-20 w-[50%] h-[35%] m-auto">
         <div className="flex flex-col h-full items-center bg-base-200 rounded-lg p-4">
         <h1 className="text-2xl font-bold pb-12 text-5xl">Getting Ready!</h1>
           <h1 className="h-5/6 text-xl">Before starting the activity, get comfortable and setup the eye-tracking solution. Once you are ready and all settled, press Continue.</h1>
