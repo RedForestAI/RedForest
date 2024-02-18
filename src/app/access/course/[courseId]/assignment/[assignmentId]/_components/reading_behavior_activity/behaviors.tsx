49d1dce6097bd6e7dd8c40c6ce1d72e44646e8b4
edavalosanaya <davalosaeduardo@gmail.com>
2024-02-18 14:05:16 -0600

    Added shallow.

--- src/app/access/course/[courseId]/assignment/[assignmentId]/_components/reading_behavior_activity/behaviors.tsx
+++ src/app/access/course/[courseId]/assignment/[assignmentId]/_components/reading_behavior_activity/behaviors.tsx
@@ -180,3 +180,57 @@ export function Deep(props: BehaviorProps) {
     </>
   );
 }
+
+export function Shallow(props: BehaviorProps) {
+  if (props.config?.pageNumber != 2) return null;
+  const [started, setStarted] = useState<boolean>(false);
+
+  useEffect(() => {
+    // @ts-ignore
+    document.getElementById("shallow_instructions").showModal();
+  }, []);
+
+  return (
+    <>
+      {started ? (
+        <>
+          <div
+            className="absolute left-[28.5%] top-[64%] h-[2.5%] w-[6%] cursor-pointer bg-red-500 opacity-50"
+            onClick={() => {
+              props.setBehaviorIndex(props.behaviorIndex + 1);
+            }}
+          ></div>
+        </>
+      ) : (
+        <>
+          {/* Instructions */}
+          <dialog id="shallow_instructions" className="modal">
+            <div className="modal-box">
+              <h3 className="text-lg font-bold">Shallow Reading Behavior</h3>
+              <p className="py-4">
+                Read the paragraph fast like you think it is less important and are not thinking about it as much. Click at the {" "}
+                <span className="bg-red-500 text-white">first word</span> when
+                you start and the {" "} 
+                <span className="bg-red-500 text-white">last word</span> {" "}
+                when you finish.{" "}
+              </p>
+              <div className="modal-action">
+                <form method="dialog">
+                  <button className="btn">Start</button>
+                </form>
+              </div>
+            </div>
+          </dialog>
+
+          {/* Initial highlight */}
+          <div
+            className="absolute left-[14%] top-[27.5%] h-[2.5%] w-[10%] cursor-pointer bg-red-500 opacity-50"
+            onClick={() => {
+              setStarted(true);
+            }}
+          ></div>
+        </>
+      )}
+    </>
+  );
+}
