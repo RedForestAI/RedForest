e632c03c03fa6e2730a338a2d4fd0af45ee83933
Eduardo Davalos Anaya <40870026+edavalosanaya@users.noreply.github.com>
2024-02-26 10:36:06 -0600

    Initial add of 3D model (#167)
    * Adding Panda to Home page.
    
    * Finished loading 3D model.

---
+++ src/app/_components/GreetingPanda.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import dynamic from 'next/dynamic'
+import { Canvas } from "@react-three/fiber"
+import { OrbitControls } from "@react-three/drei"
+
+const Panda = dynamic(() => import('@/components/threed/Panda').then((mod) => mod.Panda), { ssr: false })
+
+export default function GreetingPanda() {
+  return (
+    <>
+      <Canvas shadows camera={{ position: [0, 2, 5], fov: 30}} dpr={[1, 2]} gl={{ preserveDrawingBuffer: true}}>
+        <OrbitControls />
+        <group scale={0.5} position={[0,-0.75,0]} rotation={[0,-Math.PI * 0.2,0]}>
+          <Panda />
+        </group>
+        <ambientLight intensity={1} />
+      </Canvas>
+    </>
+  )
+}
