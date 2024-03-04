cd6182e42ec15679251458a370afb1785e4e4080
edavalosanaya <davalosaeduardo@gmail.com>
2024-03-04 16:56:24 -0600

    Added danfojs to leverage DataFrames to data manipulation.

--- src/app/access/course/[courseId]/assignment_report/[assignmentId]/activity_report/[activityId]/_components/reading/FileProcessing.ts
+++ src/app/access/course/[courseId]/assignment_report/[assignmentId]/activity_report/[activityId]/_components/reading/FileProcessing.ts
@@ -1,3 +1,4 @@
+import { get } from "http";
 import { PerStudentData } from "./types";
 
 type LogProcessingProps = {
@@ -5,19 +6,31 @@ type LogProcessingProps = {
   setPerStudentData: (perStudentData: any) => void
 }
 
-function getQuestionAnswerTimes(logs: any) {
+function getQuestionAnswerTimes(logs: any): {[key: string]: any} {
   // Get the question answer times
-
+  console.log(logs.actions)
+  return {}
 }
 
 export default function LogProcessing(props: LogProcessingProps) {
 
-  console.log(props)
+  // console.log(props)
 
   // Iterate through each student's data
   for (const studentId in props.perStudentDatas) {
     const studentData = props.perStudentDatas[studentId];
     const logs = studentData!.logs;
+
+    const results = getQuestionAnswerTimes(logs);
+
+    // Update the dataStore
+    for (const key in results) {
+      if (results.hasOwnProperty(key)) {
+        const result = results[key];
+        // @ts-ignore 
+        studentData.dataStore[key] = result;
+      }
+    }
 
   }
 }
--- src/utils/log_utils.ts
+++ src/utils/log_utils.ts
@@ -1,6 +1,8 @@
 /** Convert a 2D array into a CSV string
  * https://stackoverflow.com/a/68146412/13231446
  */
+import * as dfd from 'danfojs'
+
 export function arrayToCsv(data: any[]): string {
   return data.map(row =>
     row
@@ -13,16 +15,35 @@ export function arrayToCsv(data: any[]): string {
 
 function parseCSVData(data: string): string[][] {
   const rows = data.split('\n').filter(row => row.length > 0);
-  return rows.map(row => row.split(','));
+  const cells: string[][] = rows.map(row => row.split(','));
+
+  // remove any whitespace characters from the cells
+  for (let i = 0; i < cells.length; i++) {
+    for (let j = 0; j < (cells[i]?.length ?? 0); j++) {
+      // @ts-ignore
+      cells[i][j] = cells[i][j].trim();
+      // @ts-ignore
+      cells[i][j] = cells[i][j].replaceAll('"', '');
+    }
+  }
+  return cells
 }
 
-export function loadCSVData(blob: Blob): Promise<string[][]> {
+export function loadCSVData(blob: Blob, as_df: boolean = true): Promise<string[][] | dfd.DataFrame> {
   return new Promise((resolve, reject) => {
     const reader = new FileReader();
     reader.onload = () => {
       const data = reader.result as string;
-      const parsedData = parseCSVData(data);
-      resolve(parsedData);
+      if (as_df) {
+        const parsedData = parseCSVData(data);
+        const header = parsedData[0];
+        const body = parsedData.slice(1);
+        const danfoData = new dfd.DataFrame(body, { columns: header })
+        resolve(danfoData);
+      }
+      else {
+        resolve(parseCSVData(data));
+      }
     };
     reader.onerror = reject;
     reader.readAsText(blob);
