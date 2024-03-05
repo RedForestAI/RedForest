a0cdcb6adaee5e5ef6378c876d80d0f36e425172
edavalosanaya <davalosaeduardo@gmail.com>
2024-03-04 20:07:06 -0600

    Fixing DB issues.

--- prisma/migrations/20240305002921_adding_profile_id_and_answers_trace_to_activity_data_to_ease_the_pre_processing/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - The required column `profileId` was added to the `user_activity_data` table with a prisma-level default value. This is not possible if the table is not empty. Please add this column as optional, then populate it before making it required.
-
-*/
--- AlterTable
-ALTER TABLE "user_activity_data" ADD COLUMN     "answersTrace" JSONB[],
-ADD COLUMN     "profileId" UUID NOT NULL;
---
+++ prisma/migrations/20240305020646_added_profile_id_and_answers_trace_to_activity_data/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE "user_activity_data" ADD COLUMN     "answersTrace" JSONB[],
+ADD COLUMN     "profileId" UUID NOT NULL DEFAULT uuid_generate_v4();
